441c90e96a2bd259ff61d9366f5ea3cbafd38091
Chris Keilberg <83353741+ckeilberg57@users.noreply.github.com>
2023-04-04 13:03:29 -0400

    Update ui.js

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -38,8 +38,8 @@ var settingsButton = document.getElementById('settingsButton');
 
 //Use your PubNub account details.  Details must match PexQr companion application
 var virtualRemote = new PubNub({
-  publishKey: 'pub-c-xxxxxxxxxxxxxxxxxxxxxx',
-  subscribeKey: 'sub-c-xxxxxxxxxxxxxxxxxxxxx',
+  publishKey: 'pub-c-df7d4742-a65f-4f77-8f83-f1771c9445ba',
+  subscribeKey: 'sub-c-b8d120fe-7004-40cb-ac25-0fc682333de0',
   keepAlive: true, // Keep the connection alive
   presenceTimeout: 600, // Don't timeout for 10 minutes
   uuid: 'PexME_Receiver',
